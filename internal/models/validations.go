39c34f47268be1d8d87c505d0d8b42f6eda3c019
shtol <amir.shtol@digitaldrugs.tech>
2026-03-19 12:29:01 +0200

    returned validations

---
+++ internal/models/validations.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ValidateSurveyAdding(s Survey) error {
+	if strings.TrimSpace(s.Name) == "" {
+		return fmt.Errorf("name cannot be empty")
+	}
+	if len(s.Questions_list) == 0 {
+		return fmt.Errorf("no questions found")
+	}
+
+	for i, q := range s.Questions_list {
+		if q.Type != MultipleChoice && q.Type != TextBased {
+			return fmt.Errorf("questions_list[%d] has an incorrect question type", i)
+		}
+		if q.Type == MultipleChoice && len(q.Choices) == 0 {
+			return fmt.Errorf("questions_list[%d] with property MultipleChoice, but no choices present", i)
+		}
+		if q.Type == TextBased && len(q.Choices) > 0 {
+			return fmt.Errorf("questions_list[%d] with property TextBased is not allowed to have choices", i)
+		}
+
+		for j, choice := range q.Choices {
+			if strings.TrimSpace(choice.Description) == "" {
+				return fmt.Errorf("choice %d is empty at questions_list[%d]", j, i)
+			}
+		}
+	}
+	return nil
+}
